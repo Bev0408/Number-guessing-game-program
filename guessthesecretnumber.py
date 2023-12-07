ec09308116f47850eca65b5e0bf65b3fe2a43430
Bev <Beveridge08@gmail.com>
2023-12-07 11:05:09 +0000

    minor changes made to comments on the code

--- guessthesecretnumber.py
+++ guessthesecretnumber.py
@@ -1,5 +1,5 @@
 import random
-# welcomes the user and explains the game explain the game to user
+# welcomes the user and explains the game to user
 print("Welcome to the number guessing game! Keep attempting to guess the secret number until you get it right")
 secret_number = random.randint(1,100)
 #loops until user guesses correctly
